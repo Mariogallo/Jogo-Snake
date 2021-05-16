e36a428876ea731879a30359f4ed8dc759bf04d3
Mariogallo <prof.eng.mariogallo@gmail.com>
2021-05-16 14:27:21 -0300

    Desenvolvendo a Cobrinha

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ let canvas = document.getElementById("snake");
 let content = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-snake[0] = {
+snake[0] ={
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBg() {
     context.fillStyle = "lightgreen";
@@ -14,8 +15,32 @@ function criarBg() {
 
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
-        content
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-criarBg();
+function iniciarJogo(){
+    criarBg();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);
